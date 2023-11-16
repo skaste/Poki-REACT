ee6a7630b6edecaa217c98566dac50e672739f39
Steven D. Kaste <steven.d.kaste@Stevens-MacBook-Pro.local>
2023-11-16 14:02:26 -0600

    input the useEffect to link the API to the App.jsx through Pokilist.jsx

--- src/Components/PokiList.jsx
+++ src/Components/PokiList.jsx
@@ -1,9 +1,20 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const PokiList = () => {
   //create a useState to pull Poki and set Poki 
   const [] = useState();
   //need to create a useEffect to pull from the API and place in a try->catch function and also use async await
+  useEffect(() => {
+    async function fetchPokemon() {
+      try{
+        const response = await fetch("https://pokeapi.co/api/v2/pokemon/");
+        const result = await response.json();
+      }catch (e) {
+        console.error(error);
+      }
+    }
+    fetchPokemon();
+  })
   <>
   
   </>
