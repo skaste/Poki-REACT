9cef111bcfe960de976175f8c0d3f2e8ef8ae76a
Steven D. Kaste <steven.d.kaste@Stevens-MacBook-Pro.local>
2023-11-16 13:34:01 -0600

    working on mapping out with pseudocode

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+
 import './App.css'
 
 function App() {
-  
+  //need to create a use state that pulls the PokiList to the App.jsx parent
+  const [] = useState();
+  //need an effect that pulls info from the API to the App and place in a try->catch function and need to put async await in since pulling from API
   return (
     <>
      
