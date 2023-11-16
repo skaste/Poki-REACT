42bf27e41b73893ce1a5f5548d804d4349fdfd30
Steven D. Kaste <steven.d.kaste@Stevens-MacBook-Pro.local>
2023-11-16 14:25:52 -0600

    added useState variables and trying to logg the pokemonList to browser console

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-
+import PokiList from './Components/PokiList'
 import './App.css'
 
 function App() {
   //need to create a use state that pulls the PokiList to the App.jsx parent
-  const [] = useState();
+  
   return (
     <>
    {/* create a ternary operator statement to pull Poki */}
--- src/Components/PokiList.jsx
+++ src/Components/PokiList.jsx
@@ -1,22 +1,23 @@
 import React, { useEffect, useState } from "react";
 
-const PokiList = () => {
   //create a useState to pull Poki and set Poki 
-  const [] = useState();
+
+
+const PokiList = () => {
+const [pokemonList, setPokemonList] = useState([]);
   //need to create a useEffect to pull from the API and place in a try->catch function and also use async await
   useEffect(() => {
     async function fetchPokemon() {
       try{
         const response = await fetch("https://pokeapi.co/api/v2/pokemon/");
         const result = await response.json();
+        setPokemonList(result);
       }catch (e) {
         console.error(error);
       }
     }
     fetchPokemon();
-  })
-  <>
-  
-  </>
+  },[])
+  console.log("PokemonList: ", pokemonList);
 }
 export default PokiList;
