baf4130d2b5bf151648fba13f35bc5c131d6b9b3
Steven D. Kaste <steven.d.kaste@Stevens-MacBook-Pro.local>
2023-11-16 13:39:11 -0600

    more pseudocode

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,9 @@ import './App.css'
 function App() {
   //need to create a use state that pulls the PokiList to the App.jsx parent
   const [] = useState();
-  //need an effect that pulls info from the API to the App and place in a try->catch function and need to put async await in since pulling from API
   return (
     <>
-     
+   {/* create a ternary operator statement to pull Poki */}
     </>
   )
 }
--- src/Components/PokiList.jsx
+++ src/Components/PokiList.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 
 const PokiList = () => {
+  //create a useState to pull Poki and set Poki 
+  const [] = useState();
+  //need to create a useEffect to pull from the API and place in a try->catch function and also use async await
   <>
   
   </>
